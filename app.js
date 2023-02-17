4dde3cf2f07117e96da13c54e6cf569f0f52bfdb
Sameer Khan <66109509+sameernas22@users.noreply.github.com>
2023-02-17 10:43:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var bar = document.getElementById("bar");
+
+
+
+function fun(){
+    var links = document.getElementById("links");
+    var cross = document.getElementById("cross");
+    links.style.display = "flex";
+    cross.style.display = "block";
+
+}
+function c(){
+    var links = document.getElementById("links");
+    var cross = document.getElementById("cross");
+    links.style.display = "none";
+    cross.style.display = "none";
+}
